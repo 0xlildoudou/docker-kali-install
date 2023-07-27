4f3c63c165094a48c6e8cf3c027b3af5438ee817
lil-doudou <67382584+lil-doudou@users.noreply.github.com>
2023-07-27 16:57:15 +0200

    fix: gpg key download error

--- docker-kali-install.sh
+++ docker-kali-install.sh
@@ -299,7 +299,7 @@ do_install() {
 		$sh_c 'apt-get update -qq >/dev/null'
 		$sh_c "DEBIAN_FRONTEND=noninteractive apt-get install -y -qq $pre_reqs >/dev/null"
 		$sh_c 'install -m 0755 -d /etc/apt/keyrings'
-		$sh_c "curl -fsSL \"$DOWNLOAD_URL/linux/$lsb_dist/gpg\" | gpg --dearmor --yes -o /etc/apt/keyrings/docker.gpg"
+		$sh_c "curl -fsSL \"$DOWNLOAD_URL/linux/debian/gpg\" | gpg --dearmor --yes -o /etc/apt/keyrings/docker.gpg"
 		$sh_c "chmod a+r /etc/apt/keyrings/docker.gpg"
 		$sh_c "echo \"$apt_repo\" > /etc/apt/sources.list.d/docker.list"
 		$sh_c 'apt-get update -qq >/dev/null'
